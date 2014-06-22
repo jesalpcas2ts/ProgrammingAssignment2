c9e31fecb3e03df038e48a6cb2fbd21b18f91c12
jesalpcas2ts <jesus.alpizar@gmail.com>
2014-06-22 07:23:06 -0600

    Solution to Programming Assigment 2@

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This set of functions ilustrate how R can cache values taking adventage of the scoping capabilities 
+## 
 
-## Write a short comment describing this function
+## This function creates a list of creates a special "vector", which is really a list containing a function to
+##      set the value of the Matrix
+##      get the value of the Matrix
+##      set the value of the inverse matrix
+##      get the value of the inverse Matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve) 
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the mean of the special "Matrix" created with the makeCacheMatrix function. 
+## However, it first checks to see if the inverse has already been calculated. If so, it gets the inverse from the 
+## cache and skips the computation. Otherwise, it calculates the inverse of the matrix and sets the value of the
+## inverse in the cache via the setsolve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
